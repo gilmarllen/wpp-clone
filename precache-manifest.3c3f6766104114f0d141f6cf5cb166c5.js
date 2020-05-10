2985d4519fd06bbf76c8306ff3226fa301aaee8d
Gilmarllen <gilmarllen@gmail.com>
2020-05-10 17:59:09 -0300

    Updates

---
+++ precache-manifest.3c3f6766104114f0d141f6cf5cb166c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc30e515ab4c2fdec2c7cfc74f89af01",
+    "url": "/wpp-clone/index.html"
+  },
+  {
+    "revision": "065ba898f7764fc47805",
+    "url": "/wpp-clone/static/css/main.a797a33b.chunk.css"
+  },
+  {
+    "revision": "11222bd67b9b58309141",
+    "url": "/wpp-clone/static/js/2.a8858827.chunk.js"
+  },
+  {
+    "revision": "8dea9a823bfb24207fef47cb32cf30ad",
+    "url": "/wpp-clone/static/js/2.a8858827.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "065ba898f7764fc47805",
+    "url": "/wpp-clone/static/js/main.71502562.chunk.js"
+  },
+  {
+    "revision": "b4725f2eb53cd6f05758",
+    "url": "/wpp-clone/static/js/runtime-main.802536fb.js"
+  }
+]);
